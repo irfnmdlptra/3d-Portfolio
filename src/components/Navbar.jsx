3964ea5a01bd505b7349f2e6acaf18f0680f4436
Irfnmdlptra <bangasrat@gmail.com>
2023-04-10 13:26:27 +0800

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,7 @@ const Navbar = () => {
           window.scrollTo(0, 0);
         }}>
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+          <p className="text-white text-[18px]">Irfnmdlptra <span>| Fronend Development</span> </p>
         </Link>
     </div>
   </nav>;
