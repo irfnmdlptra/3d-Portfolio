c8834a4d75b222f902899694674ca84c87c288b7
Irfnmdlptra <bangasrat@gmail.com>
2023-04-10 12:43:57 +0800

    items-center

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   return <nav 
-  className={`${styles.paddingX} w-full flex `}>
+  className={`${styles.paddingX} w-full flex items-center `}>
 
   </nav>;
 };
