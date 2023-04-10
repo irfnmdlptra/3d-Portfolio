5ddf1a4303e9ab8458de069b29941a0ee1ec964c
Irfnmdlptra <bangasrat@gmail.com>
2023-04-10 12:48:42 +0800

    gap-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,8 @@ const Navbar = () => {
   return <nav 
   className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
     <div className="w-full flex justify-between items-center mx-w-7xl mx-auto">
-
+        <Link to="" className="flex items-center gap-2">
+        </Link>
     </div>
   </nav>;
 };
