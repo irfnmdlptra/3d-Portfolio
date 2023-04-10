3caf90ba7c96458ff8b2f3678dcbf181e445e5d2
Irfnmdlptra <bangasrat@gmail.com>
2023-04-10 10:36:19 +0800

    ad dapp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-const  App = () => {
+import { BrowserRouter } from 'react-router-dom';
 
+import { About, Contact, Experiance, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+const  App = () => {
   return (
-    <div>3d Developer Portfolio</div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/> 
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
