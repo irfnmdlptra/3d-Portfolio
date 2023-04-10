ccba13fed2d992991b15f98ff0841d7baf8799f8
Irfnmdlptra <bangasrat@gmail.com>
2023-04-10 17:35:16 +0800

    Balik lagi

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -146,7 +146,7 @@ const Customizer = () => {
           >
             <CustomButton 
               type="filled"
-              title="Go Back"
+              title="Balik lagi"
               handleClick={() => state.intro = true}
               customStyles="w-fit px-4 py-2.5 font-bold text-sm"
             />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
 
               <CustomButton 
                 type="filled"
-                title="Customize It"
+                title="Kustomin aja"
                 handleClick={() => state.intro = false}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
