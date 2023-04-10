0985ec91a92984b8d9fa709adc9eb4c1f663a9fc
Irfnmdlptra <bangasrat@gmail.com>
2023-04-10 13:54:42 +0800

    link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,16 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
           <p className="text-white text-[18px] font-bold cursor-pointer">Irfnmdlptra <span className="sm:block hidden">| Fronend Development</span> </p>
         </Link>
-        <p className="text-red-500">asdsa</p>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+        {navLinks.map((Link) => (
+          <li key={Link.id} className={`${active === Link.title
+          ? "text-white"
+          : "text-secondary"
+          } hover:text-white text-[18px] font-medium cursor-pointer`}>
+            <a href={`#${Link.id}`} >{Link.title} </a>
+          </li>
+        ))}
+        </ul>
     </div>
   </nav>;
 };
