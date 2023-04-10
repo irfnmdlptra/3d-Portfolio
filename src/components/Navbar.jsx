14e45909810787f7b6adee2340fa9f52ecc5d30f
Irfnmdlptra <bangasrat@gmail.com>
2023-04-10 12:10:29 +0800

    3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React , {useEffect, useState} from 'react'
+import { Link } from "react-router-dom";
+import
 
-import { Link } from "react-router-dom"
+
 
 const Navbar = () => {
   return (
